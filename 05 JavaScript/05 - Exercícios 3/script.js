6752b785e803dbd9b0f26a306a8b657d1cd53f98
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2022-11-03 19:34:46 -0300

    conversor de distâncias - pronto

--- 05 JavaScript/05 - Exercícios 3/script.js
+++ 05 JavaScript/05 - Exercícios 3/script.js
@@ -4,9 +4,10 @@ function converter() {
         event.preventDefault()
     });
     
+    // Lendo o input do valor em metros:
     const metros = Number(document.getElementById('metros').value);
 
-    /*
+    // Criando os cálculos de conversão:
     const medidas = {
         mm: metros * 1000,
         cm: metros * 100,
@@ -15,9 +16,46 @@ function converter() {
         hm: metros * 0.01,
         km: metros * 0.001
     }
-    */
-    let medida = getElementById('medida').value;
-    console.log(medida);
 
-    //switch ()
+    // Criando o output:
+    const saida = document.getElementById('saida');
+    let saidaParagrafo = document.createElement('p');
+    saida.innerHTML = "";
+    saida.appendChild(saidaParagrafo);
+    
+    // Lendo a opção selecionada para conversão:
+    const medida = document.getElementById('medida');
+    const escolha = medida.options[medida.selectedIndex].value;
+    //console.log(escolha);
+    
+    // Adicionando o texto dentro do output: 
+    switch (escolha) {
+        case 'mm':
+            saidaParagrafo.textContent = `Valor em milímetros: ${medidas.mm} mm`;
+            break;
+        case 'cm':
+            saidaParagrafo.textContent = `Valor em centímetros: ${medidas.cm} cm`;
+            break;
+        case 'dm':
+            saidaParagrafo.textContent = `Valor em decímetros: ${medidas.dm} dm`;
+            break;
+        case 'dam':
+            saidaParagrafo.textContent = `Valor em decâmetros: ${medidas.dam} dam`;
+            break;
+        case 'hm':
+            saidaParagrafo.textContent = `Valor em hectômetros: ${medidas.hm} hm`;
+            break;
+        case 'km':
+            saidaParagrafo.textContent = `Valor em Quilômetros: ${medidas.km} km`;
+            break;
+        default:
+            saidaParagrafo.innerText = ` Valor em milímetros: ${medidas.mm} mm
+            Valor em centímetros: ${medidas.cm} cm
+            Valor em decímetros: ${medidas.dm} dm
+            Valor em decâmetros: ${medidas.dam} dam
+            Valor em hectômetros: ${medidas.hm} hm
+            Valor em Quilômetros: ${medidas.km} km
+            `;
+            break;
+        }
 }
