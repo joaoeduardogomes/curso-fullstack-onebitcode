3f1a5f2a7cd4baa914852a7c4fc9bffd3b65f800
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2022-11-09 17:21:10 -0300

    ex 07 B - funcional

--- 05 JavaScript/07 - Exercícios 5/B/script.js
+++ 05 JavaScript/07 - Exercícios 5/B/script.js
@@ -1,3 +1,5 @@
+let valorAtual;
+
 function exibirCalculos() {
     const calculos = document.getElementById('calculos');
     calculos.classList.remove('escondido');
@@ -8,6 +10,37 @@ function encerrar() {
 }
 
 function guardarValorInicial() {
-    const valorInicial = document.getElementById('valorInicial').value;
-    console.log(valorInicial);
+    const valorInicial = Number(document.getElementById('valorInicial').value);
+
+    if (valorInicial <= 0) {
+        alert("O valor inicial deve ser maior que 0!");
+        return;
+    } else {}
+    
+    valorAtual = valorInicial;
+
+    exibirCalculos();
+    
+    exibir(valorAtual);
+}
+
+function somar() {
+    const soma = Number(document.getElementById('somar').value);
+
+    valorAtual += soma;
+
+    exibir(valorAtual);
+}
+
+function subtrair() {
+    const subtracao = Number(document.getElementById('subtrair').value);
+
+    valorAtual -= subtracao;
+
+    exibir(valorAtual);
+}
+
+function exibir(valorAtual = 0) {
+    const mostrar = document.getElementById('saida');
+    mostrar.textContent = `Valor atual: ${valorAtual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`;
 }
