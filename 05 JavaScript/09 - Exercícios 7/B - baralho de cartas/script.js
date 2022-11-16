22ab629d076f5943b6759a7484aa7b81eae7e214
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2022-11-16 16:21:01 -0300

    ex07 B - todos os naipes visíveis
    Agora qualquer carta adicionada ao baralho pode ser visualizada na tag img.

--- 05 JavaScript/09 - Exercícios 7/B - baralho de cartas/script.js
+++ 05 JavaScript/09 - Exercícios 7/B - baralho de cartas/script.js
@@ -10,7 +10,7 @@ function adicionar () {
     }
 
     const carta = valor + " " + naipe;
-    baralho.push(carta);
+    baralho.unshift(carta);
 
     exibirBaralho();
     exibirCarta("Carta adicionada:");
@@ -57,7 +57,8 @@ function urlImagemCarta() {
     let urlImagem;
     let altImagem;
 
-    switch (baralho.at(-1)) {
+    switch (baralho.at(0)) {
+        // Naipe de paus
         case "1 ♣":
             urlImagem = "https://upload.wikimedia.org/wikipedia/commons/8/87/01_of_clubs_01.svg";
             altImagem = "às de paus";
@@ -109,6 +110,165 @@ function urlImagemCarta() {
         case "K ♣":
             urlImagem = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/King_of_clubs_en.svg/209px-King_of_clubs_en.svg.png";
             altImagem = "rei de paus";
+            break;
+        // Naipe de ouros:
+        case "1 ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/1/1e/01_of_diamonds_01.svg";
+            altImagem = "às de ouros";
+            break;
+        case "2 ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/e/ea/02_of_diamonds.svg";
+            altImagem = "2 de ouros";
+            break;
+        case "3 ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/0/00/03_of_diamonds.svg";
+            altImagem = "3 de ouros";
+            break;
+        case "4 ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/8/80/04_of_diamonds.svg";
+            altImagem = "4 de ouros";
+            break;
+        case "5 ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/2/2e/05_of_diamonds.svg";
+            altImagem = "5 de ouros";
+            break;
+        case "6 ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/c/c6/06_of_diamonds.svg";
+            altImagem = "6 de ouros";
+            break;
+        case "7 ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/9/95/07_of_diamonds.svg";
+            altImagem = "7 de ouros";
+            break;
+        case "8 ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/4/42/08_of_diamonds.svg";
+            altImagem = "8 de ouros";
+            break;
+        case "9 ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/2/2c/09_of_diamonds.svg";
+            altImagem = "9 de ouros";
+            break;
+        case "10 ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/a/ae/10_of_diamonds_-_David_Bellot.svg";
+            altImagem = "10 de ouros";
+            break;
+        case "J ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Jack_of_diamonds_en.svg/209px-Jack_of_diamonds_en.svg.png";
+            altImagem = "valete de ouros";
+            break;
+        case "Q ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Queen_of_diamonds_en.svg/209px-Queen_of_diamonds_en.svg.png";
+            altImagem = "rainha de ouros";
+            break;
+        case "K ♦":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/6/66/King_of_diamonds_en.svg";
+            altImagem = "rei de ouros";
+            break;
+        // Naipe de espadas:
+        case "1 ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/a/a2/01_of_spades_01.svg";
+            altImagem = "às de espadas";
+            break;
+        case "2 ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/4/40/02_of_spades.svg";
+            altImagem = "2 de espadas";
+            break;
+        case "3 ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/6/62/03_of_spades.svg";
+            altImagem = "3 de espadas";
+            break;
+        case "4 ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/7/7a/04_of_spades.svg";
+            altImagem = "4 de espadas";
+            break;
+        case "5 ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/1/16/05_of_spades.svg";
+            altImagem = "5 de espadas";
+            break;
+        case "6 ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/6/6e/06_of_spades.svg";
+            altImagem = "6 de espadas";
+            break;
+        case "7 ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/0/06/07_of_spades.svg";
+            altImagem = "7 de espadas";
+            break;
+        case "8 ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/d/dc/08_of_spades.svg";
+            altImagem = "8 de espadas";
+            break;
+        case "9 ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/4/4f/09_of_spades.svg";
+            altImagem = "9 de espadas";
+            break;
+        case "10 ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/b/b7/10_of_spades_-_David_Bellot.svg";
+            altImagem = "10 de espadas";
+            break;
+        case "J ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Jack_of_spades_en.svg/209px-Jack_of_spades_en.svg.png";
+            altImagem = "valete de espadas";
+            break;
+        case "Q ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Queen_of_spades_en.svg/209px-Queen_of_spades_en.svg.png";
+            altImagem = "rainha de espadas";
+            break;
+        case "K ♠":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/King_of_spades_en.svg/209px-King_of_spades_en.svg.png";
+            altImagem = "rei de espadas";
+            break;
+        // Naipe de copas:
+        case "1 ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/c/c9/01_of_hearts_01.svg";
+            altImagem = "às de copas";
+            break;
+        case "2 ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/7/74/02_of_hearts.svg";
+            altImagem = "2 de copas";
+            break;
+        case "3 ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/f/fc/03_of_hearts.svg";
+            altImagem = "3 de copas";
+            break;
+        case "4 ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/2/26/04_of_hearts.svg";
+            altImagem = "4 de copas";
+            break;
+        case "5 ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/d/d1/05_of_hearts.svg";
+            altImagem = "5 de copas";
+            break;
+        case "6 ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/0/0c/06_of_hearts.svg";
+            altImagem = "6 de copas";
+            break;
+        case "7 ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/3/3f/07_of_hearts.svg";
+            altImagem = "7 de copas";
+            break;
+        case "8 ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/3/37/08_of_hearts.svg";
+            altImagem = "8 de copas";
+            break;
+        case "9 ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/e/e7/09_of_hearts.svg";
+            altImagem = "9 de copas";
+            break;
+        case "10 ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/e/e1/10_of_hearts_-_David_Bellot.svg";
+            altImagem = "10 de copas";
+            break;
+        case "J ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Jack_of_hearts_en.svg/209px-Jack_of_hearts_en.svg.png";
+            altImagem = "valete de copas";
+            break;
+        case "Q ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Queen_of_hearts_en.svg/209px-Queen_of_hearts_en.svg.png";
+            altImagem = "rainha de copas";
+            break;
+        case "K ♥":
+            urlImagem = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/King_of_hearts_en.svg/209px-King_of_hearts_en.svg.png";
+            altImagem = "rei de copas";
             break;
         default:
             urlImagem = "https://upload.wikimedia.org/wikipedia/commons/d/d0/Joker_black_02.svg";
