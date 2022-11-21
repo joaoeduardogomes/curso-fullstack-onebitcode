af3481305ff78d9a112653662815d9afd6482c49
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2022-11-21 19:10:09 -0300

    trabalhando na exibição dos imóveis

--- 05 JavaScript/10 - Exercícios 8/Cadastro de imóveis/script.js
+++ 05 JavaScript/10 - Exercícios 8/Cadastro de imóveis/script.js
@@ -1,33 +1,66 @@
 const imoveis = [];
+let formulario = document.getElementById('formulario');
 
+// Capturar inputs e salvar imóvel na lista de imóveis:
 function salvarImovel() {
     const imovel = {
         nomeProprietario: toTitleCase(document.getElementById('nome').value),
         qtdQuartos: document.getElementById('qtd-quartos').value,
         qtdBanheiros: document.getElementById('qtd-banheiros').value,
-        temGaragem: () => {
-            document.getElementsByName('garagem').value;
-            for (var i = 0; i < temGaragem.length; i++) {
-                if (temGaragem[i].checked) {
-                    // do whatever you want with the checked radio
-                    console.log(temGaragem[i].value);
-
-                    // only one radio can be logically checked, don't check the rest
-                    break;
-                }
-            }
-        }
+        temGaragem: valorRadio()
     };
 
-    /*
-    if (imovel.nomeProprietario.length === 0) {
+    // Verificando se todos os dados foram preenchidos:
+    if ((imovel.nomeProprietario.length === 0) || (imovel.qtdQuartos.length === 0) || (imovel.qtdBanheiros.length === 0)) {
+        alert("Preencha todos os campos!");
         return;
     }
-    */
+
+    imoveis.push(imovel);
 
     console.log(imovel);
+    console.log(imoveis);
+    imoveis.forEach(item => console.log(item));
 }
 
+// Verificar qual das opções foram escolhidas (radio de garagem -> s/n):
+function valorRadio() {
+    let garagem = document.getElementsByName('garagem');
+    for (let i = 0; i < garagem.length; i++) {
+        if (garagem[i].checked) {
+            // do whatever you want with the checked radio
+            return garagem[i].value;
+        }
+    }
+}
+
+// Exibir imóveis cadastrados:
+function exibirImoveis() {
+    const qtdImoveis = document.getElementById('qtd-imoveis');
+    qtdImoveis.textContent = `Quantidade de imóveis cadastrados: ${imoveis.length}.`;
+    
+    const saida = document.getElementById('saida');
+
+    const saidaImoveis = document.createElement('p');
+    saida.appendChild(saidaImoveis);
+
+    for (let i = 0; i < imoveis.length; i++) {
+        for (const item in imoveis[i]) {
+            saidaImoveis.innerText += `${item}: ${imoveis[i][item]} \n`;
+        }
+    }
+    
+    /*
+    for (let i; i < imoveis.length; i++) {
+        saidaImoveis.textContent += `Proprietário: ${imoveis[i].nomeProprietario}`
+    }
+
+    saidaImoveis.textContent = `${imoveis[0].nomeProprietario}`
+    */
+}
+
+
+// Função auxiliar:
 function toTitleCase(str) {
     return str.toLowerCase().split(' ').map(function (word) {
         return (word.charAt(0).toUpperCase() + word.slice(1));
