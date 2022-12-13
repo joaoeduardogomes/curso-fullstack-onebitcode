f6da041152cd5c2bddd5948ee6019d0e630affb4
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2022-12-13 18:08:36 -0300

    ex.12 - revisado
    Depois de revisar os conteúdos, foi possível fazer o exercício conforme a ideia inicial (levemente diferente da do instrutor)

--- 05 JavaScript/Módulo 02 - DOM/04 Exercício 12 - cadastro de devs/old/script.js
+++ 05 JavaScript/Módulo 02 - DOM/04 Exercício 12 - cadastro de devs/old/script.js
@@ -5,14 +5,13 @@ function createLabel(text, htmlFor) {
     return label
 }
 
-function createInput(id, value, name, type = 'text', placeholder = '', required) {
+function createInput(id, value, name, type = 'text', placeholder = '') {
     const input = document.createElement('input')
     input.id = id
     input.value = value
     input.name = name
     input.type = type
     input.placeholder = placeholder
-    input.required = required
     return input
 }
 
@@ -30,39 +29,21 @@ addTechBtn.addEventListener('click', function (ev) {
     newRow.id = 'inputRow-' + rowIndex
     newRow.className = 'inputRow'
 
-    //? criando o input pro datalist (tecnologias):
     const techNameLabel = createLabel('Tecnologia: ', 'techName-' + rowIndex)
-    const techNameInput = createInput('techName-' + rowIndex, null, 'techName', 'text', 'Tecnologia', true)
-    techNameInput.setAttribute('list', 'lista-tecnologias')
-    //? criando o datalist:
-    const listaDeTecnologias = document.createElement('datalist')
-    listaDeTecnologias.id = 'lista-tecnologias'
-    //? criando as opções do datalist:
-    const techHTML = document.createElement('option')
-    const techCSS = document.createElement('option')
-    const techJS = document.createElement('option')
-    const techPY = document.createElement('option')
-    techHTML.value = "HTML"
-    techCSS.value = "CSS"
-    techJS.value = "JAVASCRIPT"
-    techPY.value = "PYTHON"
-    //? Fazendo o append:
-    listaDeTecnologias.append(techHTML, techCSS, techJS, techPY)
-
+    const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
 
     // input radio do tempo de experiência: 
     const expLabel = createLabel('Experiência: ')
     const id1 = 'expRadio-' + rowIndex + '.1'
-    const expRadio1 = createInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio', "", true)
+    const expRadio1 = createInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio')
     const expLabel1 = createLabel('0-2 anos', id1)
     const id2 = 'expRadio-' + rowIndex + '.2'
-    const expRadio2 = createInput(id2, '3-4 anos', 'techExp-' + rowIndex, 'radio', "", true)
+    const expRadio2 = createInput(id2, '3-4 anos', 'techExp-' + rowIndex, 'radio')
     const expLabel2 = createLabel('3-4 anos', id2)
     const id3 = 'expRadio-' + rowIndex + '.3'
-    const expRadio3 = createInput(id3, '5+ anos', 'techExp-' + rowIndex, 'radio', "", true)
+    const expRadio3 = createInput(id3, '5+ anos', 'techExp-' + rowIndex, 'radio')
     const expLabel3 = createLabel('5+ anos', id3)
 
-    //! botão de remover:
     const removeRowBtn = document.createElement('button')
     removeRowBtn.type = 'button'
     removeRowBtn.innerHTML = '<i class="bi bi-dash-lg"></i>'
@@ -73,7 +54,7 @@ addTechBtn.addEventListener('click', function (ev) {
 
 
     newRow.append(
-        techNameLabel, techNameInput, listaDeTecnologias, expLabel, expRadio1, expLabel1, expRadio2, expLabel2, expRadio3, expLabel3, removeRowBtn
+        techNameLabel, techNameInput, expLabel, expRadio1, expLabel1, expRadio2, expLabel2, expRadio3, expLabel3, removeRowBtn
     )
 
     stackInputs.appendChild(newRow)
@@ -88,7 +69,7 @@ form.addEventListener('submit', function (ev) {
     let technologies = []
     inputRows.forEach(function (row) {
         // #rowId input[name="techName"]
-        const techName = document.querySelector('#' + row.id + ' input[name="techName"]').value.toUpperCase()
+        const techName = document.querySelector('#' + row.id + ' input[name="techName"]').value
         const techExp = document.querySelector('#' + row.id + ' input[type="radio"]:checked').value
         technologies.push({ name: techName, exp: techExp })
     })
