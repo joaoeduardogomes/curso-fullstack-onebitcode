333aea3b273d4c62041df3034b92e35098df0001
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2022-12-15 14:53:40 -0300

    iniciando a calculadora JS

---
+++ 05 JavaScript/Módulo 02 - DOM/05 Calculadora JS/index.js
@@ -0,0 +1,26 @@
+const main = document.querySelector('main');
+const root = document.querySelector(':root');
+const input = document.getElementById('input');
+const resultInput = document.getElementById('result');
+
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "];
+
+
+
+input.addEventListener('keydown', function (ev) {
+    ev.preventDefault(); // impede que seja inserido imediatamente o valor da tecla pressionada.
+    if (allowedKeys.includes(ev.key)) {
+        input.value += ev.key; // Se a tecla pressionada estiver nas 'allowedKeys', seu valor será inserido.
+        return;
+    }
+    if (ev.key === 'Backspace') {
+        input.value = input.value.slice(0, -1); // Se o 'backspace' for pressionado, o último caractere será excluído. (splice do caractere 0 até o último; por isso o '-1').
+    }
+    if (ev.key === 'Enter') {
+        calculate(); // se pressionar 'enter', o cálculo será feito.
+    }
+});
+
+function calculate() {
+    console.log("Calculado")
+}
