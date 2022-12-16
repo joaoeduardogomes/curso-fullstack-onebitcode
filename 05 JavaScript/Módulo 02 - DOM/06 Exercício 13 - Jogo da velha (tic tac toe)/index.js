83cbebe8ef9531a6267d4581ab14e3d0244d6998
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2022-12-16 18:07:49 -0300

    ex.13 - um pouco de JS aplicado

--- 05 JavaScript/Módulo 02 - DOM/06 Exercício 13 - Jogo da velha (tic tac toe)/index.js
+++ 05 JavaScript/Módulo 02 - DOM/06 Exercício 13 - Jogo da velha (tic tac toe)/index.js
@@ -1,7 +1,48 @@
 const boardRegions = document.querySelectorAll('#gameBoard span');
-const vBoard = []; //tabuleiro virtual
+let vBoard = []; //tabuleiro virtual
 let trunPlayer = '';
 
 function updateTitle() {
-    
+    const playerInput = document.getElementById(trunPlayer);
+    document.getElementById('turnPlayer').innerText = playerInput.value;
 }
+
+function initializeGame() {
+    vBoard = [['', '', ''], ['', '', ''], ['', '', '']];
+    trunPlayer = 'player1';
+    document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>';
+    updateTitle();
+    boardRegions.forEach(function (element) {
+        // Se estivermos começando um novo jogo, vamos tirar a marcação de vitória do jogo anterior:
+        element.classList.remove('win');
+        element.innerText = "" // apaga as marcações do jogo anterior
+        element.addEventListener('click', handleBoardClick); // marca uma área clicada.
+    })
+}
+
+function disableRegion(element) {
+    element.style.cursor = 'default';
+    element.removeEventListener('click', handleBoardClick);
+}
+
+function handleBoardClick(ev) {
+    const span = ev.currentTarget;
+    const region = ev.currentTarget.dataset.region; // N.N
+    // 'currentTarget' é o que acionou o evento.
+    const rowColumnPair = region.split('.'); // ["N", "N"]
+    const row = rowColumnPair[0];
+    const column = rowColumnPair[1];
+    if (trunPlayer === 'player1') {
+        span.innerText = "X";
+        vBoard[row][column] = 'X';
+    }
+    else {
+        span.innerText = "O";
+        vBoard[row][column] = 'O';
+    }
+    console.clear();
+    console.table(vBoard);
+    disableRegion(span);
+}
+
+document.getElementById('start').addEventListener('click', initializeGame);
